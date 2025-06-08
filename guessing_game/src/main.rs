316064180b9c9b2094ec3b98cedb907e6bafbb24
Mohamed Anis MANI <manimohamed@gmail.com>
2025-06-08 09:38:44 +0100

    guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret = rand::rng().random_range(1..=100);
+
+    println!("The secret number is: {}", secret);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
